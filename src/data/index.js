2b1b22d9783124752c795374fc9d20156cc1d863
kolyamba2105 <haravetskyy@gmail.com>
2023-04-16 13:57:43 +0300

    Adjust boilerplate files

---
+++ src/data/index.js
@@ -0,0 +1,5 @@
+const a = 2;
+const b = 2;
+const c = a + b;
+
+export const result = `${a} + ${b} = ${c}`;
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
+import { result } from "./data";
 import "./index.css";
-import { createElement } from "./lib";
 
-const root = document.getElementById("root");
-
-if (root) {
-  root.className = "flex items-center justify-center";
-
-  const link = createElement("a", {
-    // eslint-disable-next-line prettier/prettier
-    className: "text-4xl text-sky-600 underline transition-all delay-100 duration-200 ease-in hover:text-sky-400",
-    href: "https://github.com/kolyamba2105/webpack-vanilla-template",
-    innerText: "Webpack Template",
-    rel: "noreferrer",
-    target: "_blank",
-  });
-
-  root.appendChild(link);
-}
+alert(result);
